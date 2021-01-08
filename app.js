7b093d53fc5681ecb34e2baf293d746e1bdba3a9
Sourav-Patel <er.patelsourav@gmail.com>
2021-01-09 01:01:47 +0530

    basic layout added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts')
+
 // const routes = require('./src/routes')
+
 const { SERVER_PORT } = require('./config/config')
 
 // Connect to mongodb
@@ -11,11 +14,21 @@ require('./db')
 
 const app = express()
 
+app.use(express.static('public'))
+
+app.use(expressLayouts)
+app.set('layout', 'layouts/layout')
+app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/ping', (req, res) => {
     res.json({
         msg: 'success'
     })
+})
+
+app.get('/', function (req, res) {
+    res.render('home')
 })
 // app.use('/api', routes)
 app.listen(SERVER_PORT, (err) => {
